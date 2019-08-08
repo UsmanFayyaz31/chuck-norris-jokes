5da769cf711efea805f792fcff65065b3af6c487
Usman Fayyaz <usman.fayyaz@ucp.edu.pk>
2019-08-08 12:29:06 +0500

    deploying

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import titlePic from './logo.jpg';
+
+class QuoteMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: ""
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.getNewContent = this.getNewContent.bind(this);
+  }
+  handleClick(event) {
+    this.getNewContent();
+  }
+
+  getNewContent(){
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then(response => response.json())
+      .then(dat => this.setState({ quote: dat.value }));
+  }
+
+  componentDidMount() {
+    this.getNewContent();
+  }
+
+  render() {
+    return(
+      <div id="container">
+        <img id="title" src={titlePic} alt="logo"></img>
+        <h3>Chuck Norris jokes</h3>
+        <div id="quote-box">
+          <p id="text">{this.state.quote}</p>
+          <button id="new-quote" onClick={this.handleClick}>Next Joke</button>
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <QuoteMachine />
   );
-}
+};
 
 export default App;
