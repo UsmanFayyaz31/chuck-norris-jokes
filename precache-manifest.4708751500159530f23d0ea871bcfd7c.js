2d8c94e17d4c922102989d08522878a4041c5960
Usman Fayyaz <usman.fayyaz@ucp.edu.pk>
2019-08-08 12:47:33 +0500

    Updates

---
+++ precache-manifest.4708751500159530f23d0ea871bcfd7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6cdc439923da34b935a5d8173bab03",
+    "url": "/chuck-norris-jokes/index.html"
+  },
+  {
+    "revision": "3161d611fb38fdc29268",
+    "url": "/chuck-norris-jokes/static/css/main.0fa904b1.chunk.css"
+  },
+  {
+    "revision": "9b3a5007987034dff77a",
+    "url": "/chuck-norris-jokes/static/js/2.652dea17.chunk.js"
+  },
+  {
+    "revision": "3161d611fb38fdc29268",
+    "url": "/chuck-norris-jokes/static/js/main.0c55a704.chunk.js"
+  },
+  {
+    "revision": "e46a7c2959dbe03c59f8",
+    "url": "/chuck-norris-jokes/static/js/runtime~main.c8cb4fa3.js"
+  },
+  {
+    "revision": "fcc5a2c4a309d367a036eafca8e06811",
+    "url": "/chuck-norris-jokes/static/media/logo.fcc5a2c4.jpg"
+  }
+]);
