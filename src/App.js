93e0418d267b2dfc55025cf3b3b101dd0246caf2
Usman Fayyaz <usman.fayyaz@ucp.edu.pk>
2019-08-28 15:33:56 +0500

    add author

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class QuoteMachine extends React.Component {
     this.getNewContent();
   }
 
-  getNewContent(){
+  getNewContent() {
     fetch("https://api.chucknorris.io/jokes/random")
       .then(response => response.json())
       .then(dat => this.setState({ quote: dat.value }));
@@ -27,13 +27,19 @@ class QuoteMachine extends React.Component {
   }
 
   render() {
-    return(
-      <div id="container">
-        <h1 id ="title-msg">Chuck Norris jokes</h1>
-        <img id="title-img" src={titlePic} alt="logo"></img>
-        <div id="quote-box">
-          <p id="text">{this.state.quote}</p>
-          <button id="new-quote" onClick={this.handleClick}>Next Joke</button>
+    return (
+      <div>
+        <div id="container">
+          <h1 id="title-msg">Chuck Norris jokes</h1>
+          <img id="title-img" src={titlePic} alt="logo"></img>
+          <div id="quote-box">
+            <p id="text">{this.state.quote}</p>
+            <button id="new-quote" onClick={this.handleClick}>Next Joke</button>
+          </div>
+        </div>
+        <div id="author">
+          <p>Designed and Coded by</p> <br />
+          <a href="https://github.com/UsmanFayyaz/chuck-norris-jokes">Usman Fayyaz</a>
         </div>
       </div>
     );
@@ -41,7 +47,7 @@ class QuoteMachine extends React.Component {
 }
 
 function App() {
-  return(
+  return (
     <QuoteMachine />
   );
 };
